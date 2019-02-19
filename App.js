7b5d568157f7dfab1493aeb99135eaa408768082
HarringtonR <ross.ludeke@gmail.com>
2019-02-18 18:02:48 -0600

    navigator

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
 import {
-    createBottomTabNavigator,
-    createAppContainer
-  } from 'react-navigation';
+  createMaterialTopTabNavigator,
+  createAppContainer
+} from 'react-navigation';
 import News from './src/components/News.js';
 import Chat from './src/components/Chat.js';
 
-const RootStack = createBottomTabNavigator(
-    {
-      News: {
-          screen: News
-      },
-      Chat: {
-          screen: Chat
-      }
-    });
+const RootStack = createMaterialTopTabNavigator({
+  News: News,
+  Chat: Chat
+}, {
+  tabBarPosition: 'bottom',
+  initialRouteName: 'News',
+  swipeEnabled: true,
+  animationEnabled: true,
+  tabBarOptions: {
+    activeTintColor: '#FFFFFF',
+    inactiveTintColor: '#FFFFFF',
+    style: {
+        backgroundColor: '#303030',
+    }
+  },
+});
 
 const App = createAppContainer(RootStack);
 export default App;
